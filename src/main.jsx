4f97328bd45b218a3cad55c4e24f4fa591dd1386
Melusi5 <melusimngomezulu5@gmail.com>
2023-05-09 10:37:10 +0200

    added code ffrom app.js and main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
+
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import AboutUs from './Pages/AboutUs'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+  {
+    path: "/about",
+    element: <AboutUs/>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
