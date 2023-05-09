71b4759b7c17a3ad06c2ad28b657c4f3439703b6
Melusi5 <melusimngomezulu5@gmail.com>
2023-05-09 10:47:44 +0200

    fix my project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
 import Home from './Pages/Home'
 import Contact from './Pages/Contact'
